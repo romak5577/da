87bd971ba712773fffec1680493a1c7e59f43071
romak5577 <roma.play.kozak@gmail.com>
2026-04-25 13:26:28 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+import time
+
+sock = socket(AF_INET,SOCK_STREAM)
+
+conn_ids = {}
+
+def handle_Data():
+    global id_conuter
+    while True
+
+
+    for connl in playr_data:
+        if connl in  eliminated: continue
+        p1 = player_data
+
+parts = data.splir
